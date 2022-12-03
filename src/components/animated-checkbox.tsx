9670731aa62607ff42d7fbca03b6c2a80f3be090
aalsaedi265 <aalsaedi265@gmail.com>
2022-12-02 21:25:38 -0600

    fixex babel and easing breizer problem, it was becuase of new syntax and library location

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, memo } from 'react'
-import { Pressable } from 'react-native'
+import { Pressable,Easing  } from 'react-native'
 import { Text, HStack, Box } from 'native-base'
 import Animated, {
-  Easing,
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   withSequence,
   withDelay,
-  interpolateColor
+  interpolateColor,
+  useAnimatedProps
 } from 'react-native-reanimated'
 import Svg,{ Path, Defs, ClipPath, G } from 'react-native-svg'
 
@@ -20,26 +20,60 @@ const vHeight = 64+ Margin
 
 let box_drawing= "M24 0.5H40C48.5809 0.5 54.4147 2.18067 58.117 5.88299C61.8193 9.58532 63.5 15.4191 63.5 24V40C63.5 48.5809 61.8193  54.4147 58.117 58.117C54.4147 61.8193 48.5809 63.5 40 63.5H24C15.4191 63.5 9.58532 61.8193 5.88299 58.117C2.18067 54.4147 0.5 48.5809 0.5 40V24C0.5 15.4191 2.18067 9.58532 5.88299 5.88299C9.58532 2.18067 15.4191 0.5 24 0.5Z"
 let check_drawing ="M8 32.5C18 39 26 47 26 47C26 47 33 28 63.5 4" 
-// viewBox={[-Margin, -Margin, vWidth+ Margin, vHeight + Margin].join(' ')}
-   
-   const AnimatedCheckBox = ()  =>{
 
-    // const {strikeThrough, textColor, inactiveTextColor, onPress, children}= props
+const AnimatedPath = Animated.createAnimatedComponent(Path)
+interface Props{
+    checked?:boolean
+}
+
+   const AnimatedCheckBox = (props:Props)  =>{
+
+    const {checked} = props
+
+    const checkmarkColor ='#000000'
+    const highlightColor ='#ff0000'
+    const boxOutlineColor='#000000'
+
+    const progress = useSharedValue(0)
+
+    useEffect(()=>{
+        progress.value = withTiming(checked? 1: 0,{
+            //duration: Easing.linear
+            easing:Easing.linear
+        })
+    },[checked])
+
+    const animatedBox_theProp= useAnimatedProps(()=>({
+                stroke: interpolateColor(
+                Easing.bezier(0.16,1,0.3,1)(progress.value),
+                [0,1],
+                [boxOutlineColor, highlightColor], 'RGB'
+                ),
+                fill:interpolateColor(
+                    Easing.bezier(0.16,1,0.3,1)(progress.value),
+                    [0,1],
+                    ['#00000000', highlightColor], 'RGB'
+                )
+        }),[highlightColor,boxOutlineColor]
+    )
+
     return(
         <Svg viewBox={[-Margin, -Margin, vWidth+ Margin, vHeight + Margin].join(' ')} >  
             
             <Path fill="none"
              d= {box_drawing}
-             stroke="black"/> 
-             
+             stroke="black"/>
+
+             <AnimatedPath d={box_drawing}
+             strokeWidth={7}
+             strokeLinejoin='round'
+             strokeLinecap='round'
+             animatedProps={animatedBox_theProp} />
+
             <Path fill="none" 
             d={check_drawing}
             stroke="black"/>
         </Svg>
-        // <>
-        // <Text>soskay aizen</Text>
-        // </>
-        
         
     )
    }
